690c8e4f4e3b907aafe6c03256846a1624954151
Jacques Bodin-Hullin <j.bodinhullin@monsieurbiz.com>
2023-01-31 15:21:08 +0100

    Sylius setup as easy as done!

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Sylius Setup
+
+## Howto
+
+- Use this template project as a new project in Github.
+- Edit the `.gitignore` file to remove unwanted lines.
+- Edit the `.php-version` as needed.
+- Run `symfony composer create-project --no-scripts sylius/sylius-standard apps/sylius`.
+- Run `make install` to get a clean setup.
+- Replace `MAILER_DSN=null://null` by `MAILER_DSN=smtp://localhost:1025` in `apps/sylius/.env`.
+- And code!
+
+## License
+
+Please see LICENSE.txt.
---
+++ resources/makefiles/development.mk
@@ -0,0 +1,40 @@
+###
+### DEVELOPMENT
+### ¯¯¯¯¯¯¯¯¯¯¯
+
+ifeq (${BUILD_THEME_IN_DOCKER},0)
+define yarn
+    (cd ${APP_DIR}; yarn $(1))
+endef
+else
+define yarn
+	$(call docker-compose,run --rm --user node node sh -c "cd ${APP_DIR}; yarn $(1)")
+endef
+endif
+
+install: docker.pull docker.build platform sylius ## Install the project
+.PHONY: install
+
+up: docker.up server.start ## Up the project (start docker, start symfony server)
+stop: server.stop docker.stop ## Stop the project (stop docker, stop symfony server)
+down: server.stop docker.down ## Down the project (removes docker containers, stop symfony server)
+
+reset: docker.down ## Stop docker and remove dependencies
+	rm -rf .cache .node-gyp .yarn
+	rm -rf ${APP_DIR}/node_modules
+	rm -rf ${APP_DIR}/vendor
+	rm -rf ${APP_DIR}/var/cache
+.PHONY: reset
+
+dependencies: .php-version ${APP_DIR}/vendor ${APP_DIR}/node_modules ## Setup the dependencies
+.PHONY: dependencies
+
+${APP_DIR}/vendor: ${APP_DIR}/composer.lock ## Install the PHP dependencies using composer
+	$(call symfony.composer,install --prefer-dist)
+
+${APP_DIR}/composer.lock: ${APP_DIR}/composer.json
+
+yarn.install: ${APP_DIR}/node_modules
+
+${APP_DIR}/node_modules: ${APP_DIR}/yarn.lock
+	$(call yarn,install)
